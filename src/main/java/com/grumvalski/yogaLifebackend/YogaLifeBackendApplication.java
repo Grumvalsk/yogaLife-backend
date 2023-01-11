13f87656a8dd3e6f85b4e70a516f3d03fcc8bbe7
Tommaso <tommymariottyrosso@hotmail.it>
2023-01-11 17:36:34 +0100

    master

---
+++ src/main/java/com/grumvalski/yogaLifebackend/YogaLifeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grumvalski.yogaLifebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaLifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaLifeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grumvalski/yogaLifebackend/YogaLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grumvalski.yogaLifebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaLifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
